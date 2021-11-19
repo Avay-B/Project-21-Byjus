cff62f8ae5d87c309d35650fb5ddec53ed58b94c
Avay-B <81188923+Avay-B@users.noreply.github.com>
2021-11-19 14:48:02 +0545

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 900);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+var paper_ball;
+var ball_options ={
+	
+	isStatic:false
+	restitution:0.3
+	friction:0
+	density:1.2
+
+}
+
+groundObj = new ground(width/2670,width, 20);
+leftSide = new ground(1100, 600, 20, 120);
+rightSide = new ground(1300,600,20,120);
+
+}
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if (keyCode == UP_ARROW){
+		Matter.Body.applyForce(paper_ball, position, force);
+	}
+}
+
